4dc05d889672e6a309e7b7e0cd53a93a5487e2a4
imatellan <ivan.matellan@snoopconsulting.com>
2015-10-02 13:22:20 -0300

    Recuperar Branch name git

--- src/main/java/com/snoop/dev2/Main.java
+++ src/main/java/com/snoop/dev2/Main.java
@@ -1,6 +1,9 @@
 package com.snoop.dev2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutionException;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -15,6 +18,8 @@ public class Main {
 			File file = new File("/home/imatellan/git/NexusFidelizacion/pom.xml");
 			JAXBContext jaxbContext = JAXBContext.newInstance(Project.class);
 
+			String newBranch = executeCommand("git rev-parse --abbrev-ref HEAD");
+			System.out.println(newBranch);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 			Project pom = (Project) jaxbUnmarshaller.unmarshal(file);
 			System.out.println(pom.version);
@@ -25,4 +30,21 @@ public class Main {
 
 		}
 	
+	public static String executeCommand(String command) {
+	    StringBuffer output = new StringBuffer();
+	    Process p;
+	    try {
+	        p = Runtime.getRuntime().exec(command);
+	        p.waitFor();
+	        BufferedReader reader = 
+	                        new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        String line = "";           
+	        while ((line = reader.readLine())!= null) {
+	            output.append(line + "\n");
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return output.toString();
+	}
 }
